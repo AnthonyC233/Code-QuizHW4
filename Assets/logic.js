b2a6487c37f20cdaa5796d6b5115ac745547d82a
AnthonyC233 <changanthony61@yahoo.com>
2022-04-11 12:57:22 -0400

    updated index, js, and css

--- Assets/logic.js
+++ Assets/logic.js
@@ -1,15 +1,44 @@
-var startButton = document.getElementById('start-btn')
+var Questions = [
+    {
+        title:"What are carrot symbols?",
+        choices: ["''","{}","<>","&&"],
+        answer:"<>"
+    },
+    {
+        title:"What does .slice do?",
+        choices: ["appends new element","covers a section of an array","is a universal element","self closing tag"],
+        answer:'covers a section of an array'
+    },
+    {
+        title:"What is the first thing you do to debug?",
+        choices: ["CSS","HTML","console.log","git bash"],
+        answer:"console.log"
+    }
+];
 
+
+var startButton = document.getElementById('start');
+var questionContainerElement = document.querySelector(".container");
+var startScreen = document.getElementById('start-screen');
+var questionsText = document.getElementById('question-text');
+var answerButtons = document.querySelectorAll('#choices .btn');
+var currentQuestionIndex = 0;
 
 let score = 0;
 
 startButton.addEventListener('click',beginGame)
 
 function beginGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    showQuestion(Questions[0].Questions)
-}
+    console.log('Started');
 
+    startScreen.classList.add('hide');
+    questionContainerElement.classList.remove('hide');
+    showQuestion();
+};
+
+console.log(Questions[currentQuestionIndex])
+
+function showQuestion() {
+    questionsText.textContent =Questions[currentQuestionIndex].title;
+    
+}
